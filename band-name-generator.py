e52874bf8998490d4d34b3a8c1a108411c644d4f
Divya Akkiraju <106351768+divyaakkiraju@users.noreply.github.com>
2022-12-16 10:33:56 -0800

    Upload band-name-generator.py
    Project for Day 1 of course

---
+++ band-name-generator.py
@@ -0,0 +1,11 @@
+# Greeting for program.
+print("Welcome to the band name generator.")
+
+# Prompt user to input city they grew up in.
+city = input("Which city did you grow up in?\n")
+
+# Prompt user to input childhood pet name.
+pet = input("What was the name of your childhood pet?\n")
+
+# Band name generated from city and childhood pet. 
+print("Your band name could be " + city + " " + pet + "!")
